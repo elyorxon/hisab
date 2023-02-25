3b805cefb015e44dfe2fc88de98e15737db5d1c5
elyorxon <68835686+elyorxon@users.noreply.github.com>
2023-02-25 16:59:28 +0500

    added expenses html

--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
-from .forms import ProductForm
-from .models import Product
+from .forms import ProductForm, ExpensesForm
+from .models import Product, Expenses
 
 
 def homepage(request):
@@ -37,26 +37,27 @@ class ProductListView(ListView):
 
 
 class ExpenseCreateView(CreateView):
-    model = Product
-    form_class = ProductForm
+    model = Expenses
+    form_class = ExpensesForm
     template_name = 'core/expense_add.html'
     success_url = reverse_lazy('core:expense_list')
 
 
 class ExpenseUpdateView(UpdateView):
-    model = Product
-    form_class = ProductForm
+    model = Expenses
+    form_class = ExpensesForm
     template_name = 'core/expense_update.html'
     success_url = reverse_lazy('core:expense_list')
 
 
 class ExpenseDeleteView(DeleteView):
-    model = Product
+    model = Expenses
+    form_class = ExpensesForm
     template_name = 'core/expense_delete.html'
     success_url = reverse_lazy('core:expense_list')
 
 
 class ExpenseListView(ListView):
-    model = Product
-    template_name = 'core/product_list.html'
-    context_object_name = 'products'
+    model = Expenses
+    template_name = 'core/expense_list.html'
+    context_object_name = 'expenses'
