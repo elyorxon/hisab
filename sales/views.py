f603f19f53f3d51d60aa477ef5fa1681ac479784
elyorxon <68835686+elyorxon@users.noreply.github.com>
2023-03-04 21:31:30 +0500

    added customer models

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
-# Create your views here.
+from .forms import CustomerForm
+from .models import Customer
+
+
+def homepage(request):
+    return render(request=request, template_name="core/index.html")
+
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    form_class = CustomerForm
+    template_name = 'sales/customer_add.html'
+    success_url = reverse_lazy('sales:customer_list')
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    form_class = CustomerForm
+    template_name = 'sales/customer_update.html'
+    success_url = reverse_lazy('sales:customer_list')
+
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    template_name = 'sales/customer_delete.html'
+    success_url = reverse_lazy('sales:customer_list')
+
+
+class CustomerListView(ListView):
+    model = Customer
+    template_name = 'sales/customer_list.html'
+    context_object_name = 'customers'
+
